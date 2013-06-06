09cfecba1bf022cd9c1917663697fb7d263b0b54
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-06-06 15:20:22 +0400

    Chest data definition, first version

--- sql/ch.defineProperties.sql
+++ sql/ch.defineProperties.sql
@@ -4,3 +4,13 @@ ch.defineProperties
 ch.defineProperties
 'name,STRING:code,STRING:address,STRING:xmlData,xml:altitude,double:longitude,double'
 ;
+ch.defineProperties
+'dateb,datetime:datee,datetime:status,integer'
+;
+ch.defineProperties
+'title,STRING:fname,STRING:lname,STRING:photo,long binary:UOAuthAccount,ID'
+;
+
+
+commit
+;
